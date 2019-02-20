c5bced71eaad692cdd4f18caa6eb649a0d446b7c
fikrybahaswan <bahaswanfikry@gmail.com>
2019-02-20 21:14:35 +0700

    tugas praktikum 3

---
+++ Latihan2.py
@@ -0,0 +1,11 @@
+print("***** Latihan 2 *****")
+print("Menampilkan bilangan, berhenti ketika bilangan 0, dan menampilkan bilangan terbesar")
+
+max = 0
+while True:
+    a = int(input("Masukkan Bilangan: "))
+    if max < a :
+        max = a
+    if a==0:
+        break
+print("Bilangan terbesarmya adalah = ",max)
